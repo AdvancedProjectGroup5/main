547bd3f295917dc6fc0e5e62d8e030a9ddfcd975
Cúán Tilson <cuantilson@gmail.com>
2024-11-25 15:40:24 +0200

    Refactor App component to implement routing and add NavBar and Footer

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import logo from './assets/logo.svg';
-import './App.css';
-import axios from 'axios';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar"; // Import the NavBar component
+import Home from "./screens/Home"; // Import your screens
+import Movies from "./screens/Movies";
+import Reviews from "./screens/Reviews";
+import Profile from "./screens/Profile";
+import LoginSignUp from "./screens/LoginSignUp";
+import Showtimes from "./screens/Showtimes";
+import Groups from "./screens/Groups";
+import Footer from "./components/Footer.js"; // Import the Footer component
+import "./App.css";
 
-const url = 'http://localhost:3001'
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React awdawd
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar /> {/* NavBar will appear on all pages */}
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Route for Home */}
+          <Route path="/movies" element={<Movies />} /> {/* Route for Movies */}
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<LoginSignUp />} />
+          <Route path="/showtimes" element={<Showtimes />} />
+          <Route path="/groups" element={<Groups />} />
+        </Routes>
+      </div>
+      <Footer /> {/* Footer will appear on all pages */}
+    </Router>
   );
-}
+};
 
 export default App;
