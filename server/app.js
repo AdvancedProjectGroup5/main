fd9f46c5b523bbdbc90e0bacc76cf7c3827cc717
t3mina00 <nmine23@student.oulu.fi>
2024-11-26 22:18:43 +0900

    Add auth endpoint

--- server/app.js
+++ server/app.js
@@ -3,17 +3,25 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocs from './api-doc/swaggerConfig.js';
+import cookieParser from 'cookie-parser';
 import errorHandler from './middlewares/errorHandler.js';
+import responseHelpers from './middlewares/responseHelpers.js';
+import userRouter from './routes/UserRouter.js';
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(cors())
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
 app.use(express.json())
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.status(200).json({result: "Success"})
 })
+
+app.use(responseHelpers);
+
+app.use('/auth', userRouter);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(errorHandler);
---
+++ server/middlewares/responseHelpers.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken'
+const { sign } = jwt
+
+const responseHelpers = (req, res, next) => {
+    res.exposeHeaders = () => {
+        return res.header('Access-Control-Expose-Headers','Authorization,Set-Cookie')
+    }
+    
+    res.authorizationHeader = (email) => {
+        const access_token = sign({user: email}, process.env.JWT_SECRET_KEY, {expiresIn: '15m'})
+        return res.header('Authorization','Bearer ' + access_token)
+    }
+
+    res.refreshToken = (email) => {
+        const refresh_token = sign({user: email}, process.env.JWT_SECRET_KEY, {expiresIn: '30d'})
+        
+        return res.cookie('refreshToken',refresh_token,{
+            maxAge: 30 * 24 * 60 * 60 * 1000
+        })
+    }
+
+    next()
+}
+
+export default responseHelpers
