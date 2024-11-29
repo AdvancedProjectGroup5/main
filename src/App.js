60d7c539f2ffc9eefe681edbfbaf4569e7c4c67a
hab1b01 <t3ulmo00@students.oamk.fi>
2024-11-29 19:27:36 +0545

    added auth feature

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import logo from './assets/logo.svg';
-import './App.css';
-import axios from 'axios';
-
-const url = 'http://localhost:3001'
-
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./screens/signin.jsx";
+import SignUp from "./screens/signup.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <h1>Profile</h1>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthProvider>
+    </Router>
   );
-}
+};
 
 export default App;
