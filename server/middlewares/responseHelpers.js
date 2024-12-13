361d9e04411c3aada9eda8347e30114de215234d
t3mina00 <nmine23@student.oulu.fi>
2024-12-14 00:48:38 +0200

    Add setting to send cookie

--- server/middlewares/responseHelpers.js
+++ server/middlewares/responseHelpers.js
@@ -15,7 +15,8 @@ const responseHelpers = (req, res, next) => {
         const refresh_token = sign({user: email}, process.env.JWT_SECRET_KEY, {expiresIn: '30d'})
         
         return res.cookie('refreshToken',refresh_token,{
-            maxAge: 30 * 24 * 60 * 60 * 1000
+            maxAge: 30 * 24 * 60 * 60 * 1000,
+            sameSite: 'None'
         })
     }
 
